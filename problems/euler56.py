9be001b94ab47cfb70ee9043b61649e4b6da073f
AdamW <adam1edinburgh@gmail.com>
2019-06-06 16:24:22 +0300

    added desc to 56

--- problems/euler56.py
+++ problems/euler56.py
@@ -1,3 +1,10 @@
+#_______Description_____
+
+#https://projecteuler.net/problem=56
+#A googol (10100) is a massive number: one followed by one-hundred zeros; 100100 is almost unimaginably large: one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only 1.
+#Considering natural numbers of the form, ab, where a, b < 100, what is the maximum digital sum?
+##_______end_description_____:
+
 def count_digits(num):
     """sum digits in number"""
     stringy = str(num)
